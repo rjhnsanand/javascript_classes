dbe21f13eb342e68793fda5577f121c471399dc4
rjhnsanand <157540685+rjhnsanand@users.noreply.github.com>
2024-01-30 12:18:51 +0000

    working with variables

---
+++ basics.js
@@ -0,0 +1,9 @@
+console.log("Anand")
+
+// To add comment
+// Semicolon ; is not necessary
+//Code should be readable
+console.log("Anand")
+console.log(23);
+
+// Constants and variables
---
+++ variables.js
@@ -0,0 +1,30 @@
+// what is the need?
+
+// Data stored using constant can't be changed, it's fixed.
+// Data stored with variable can be changed
+
+// Camel case is when the first alphabet of the second word is capitalised. 
+
+const userId = 2345
+let userName = "Anand"
+var userPassword = "Apple123"
+userCity = "Patna"
+
+console.log(userName);
+
+// const is used to refer Constants, which cannot be changed later.
+// let and var are variable.
+// var should not be used, let should be used all the time.
+/* like in userCity if you don't declare a type of variable, javascript will still count it as a variable, it is not recommended though.
+Always declare the type of variable initially you wan to work with */
+
+userName = "Pandey"
+userPassword = "Fortune123"
+userCity = "Bangalore"
+
+console.table([userId, userName, userPassword, userCity]);
+
+
+
+
+
