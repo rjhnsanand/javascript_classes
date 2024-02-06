a8e46e6b9c75f14e5f000be9f58f471955610b0f
rjhnsanand <157540685+rjhnsanand@users.noreply.github.com>
2024-02-06 16:21:21 +0000

    added test

---
+++ test.js
@@ -0,0 +1,11 @@
+
+//const twitterUser = new Object()
+
+
+const twitterUser = {}
+
+twitterUser.name = "Anand",
+twitterUser.email = "anandrjhns@gmail.com",
+twitterUser.password = 12345
+
+console.log(twitterUser);
