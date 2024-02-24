a9f257070d3c30c18bf805d3c0b04eecd379b8ca
rjhnsanand <157540685+rjhnsanand@users.noreply.github.com>
2024-02-24 03:50:01 +0000

    project building

---
+++ projects/colorChanger/colorChanger.js
@@ -0,0 +1,7 @@
+// Logic for the project
+// Create a mouse click event
+// loop the list and run the function to catch the color of boxes
+// Function should pick the color from list and append it to body
+
+let canvas = document.querySelector('.canvas')
+console.log(canvas)
